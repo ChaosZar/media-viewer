7068f6fa4ede1938a16e17de096c00c79cf65374
Alexander Will <alwill101@gmail.com>
2021-05-22 18:42:07 +0200

    init

---
+++ server/src/main/java/org/rynios/media/viewer/picture/PictureApi.java
@@ -0,0 +1,10 @@
+package org.rynios.media.viewer.picture;
+
+import org.openapi.example.api.MediaApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PictureApi implements MediaApi {
+
+
+}
